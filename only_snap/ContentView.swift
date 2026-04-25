e3ce5ea04174411f8d7f441a36a9ca824e456dd5
dpan538 <dpan53853@gmail.com>
2026-04-25 20:44:21 +1000

    Initial Commit

---
+++ only_snap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  only_snap
+//
+//  Created by Jarl Giovanni on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ only_snap/only_snapApp.swift
@@ -0,0 +1,17 @@
+//
+//  only_snapApp.swift
+//  only_snap
+//
+//  Created by Jarl Giovanni on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct only_snapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
